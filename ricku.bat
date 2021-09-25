51246995dee6b088681c21dabbb94ccf127b8b4d
Jeamie <64043122+Jeamie@users.noreply.github.com>
2021-09-24 21:15:06 -0400

    Add files via upload

---
+++ ricku.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/jeamie/ricku
+cd ricku
+cmd.exe /c start msedge.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ && FOR /L %%N IN () DO setvol 100
---
+++ rickulinux.sh
@@ -0,0 +1,2 @@
+i=0
+xdg-open 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' && while [ $i -le 1 ]; do amixer set Master on 100%; done 
